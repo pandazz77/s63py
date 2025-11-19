bf8eb71a17cb20244d8dbff778ddc9c96b4ec040
Denis Kalinin <kdv@shipbuilders.ru>
2025-11-19 08:29:10 +0300

    bump version

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ cmake_extension = CMakeExtension(name="s63py.s63py")
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="s63py",
-    version="0.2",
+    version="0.3",
     author="pandazz7",
     author_email="kalin1538inn@gmail.com",
     description="python bindings of s63lib",
